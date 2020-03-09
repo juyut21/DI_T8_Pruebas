375a1d954da38753d88f6175997e977d9749ea1e
Carlos <46779781+juyut21@users.noreply.github.com>
2020-03-09 09:21:51 +0100

    Entrega de ejercicios 10/03/2020

--- NumeroMax/src/junit/NumeroMax.java
+++ NumeroMax/src/junit/NumeroMax.java
@@ -1,36 +1,27 @@
-
 package junit;
 
 public class NumeroMax
 {
-    public static int max(int[] a) {
-        int i, m = 0, x = 0;
-        char operador = '>';
-        
-        if(a == null) {
+
+    public static int max(int[] a)
+    {
+        int i, m = 0;
+
+        if (a == null)
+        {
+            return 0;
+        } else if (a.length == 0)
+        {
             return 0;
         }
         
-        for (i = 0; i < a.length; i++) {
-            if(a[i] < 0) {
-                x++;
-                if(x == a.length) {
-                    operador = '<';
-                } else {
-                    operador = '>';
-                }
+        m = a[0];
+        for (i = 0; i < a.length; i++)
+        {
+            if (a[i] > m)
+            {
+                m = a[i];
             }
-        }
-            
-        for (i = 0; i < a.length; i++) {
-            if(operador == '>') {
-                if (a[i] > m) 
-                    m = a[i];
-            } else {
-                if (a[i] < m) 
-                    m = a[i];
-            }
-                   
         }
         return m;
     }
--- NumeroMax/test/junit/NumeroMaxTest.java
+++ NumeroMax/test/junit/NumeroMaxTest.java
@@ -29,10 +29,18 @@ public class NumeroMaxTest
         assertEquals(9, NumeroMax.max(new int [] {3, 5, 9}));
         assertEquals(5, NumeroMax.max(new int [] {4, 3, 5}));
         assertEquals(5, NumeroMax.max(new int [] {5, 4, 3}));
-        assertEquals(-9, NumeroMax.max(new int [] {-9, -4, -3}));
+        assertEquals(-3, NumeroMax.max(new int [] {-9, -4, -3}));
+        assertEquals(1, NumeroMax.max(new int [] {1}));
         assertEquals(0, NumeroMax.max(new int [] {}));
         assertEquals(0, NumeroMax.max(null));
         
+    }
+    
+    @Test(timeout=1000)
+    public void testMaxTiempo()
+    {
+        System.out.println("Pruebas JUnit NumeroMax Tiempo");
+        assertEquals(9, NumeroMax.max(new int [] {3, 5, 2, 7, 5, 8, 3, 7 ,8 ,9}));
     }
     
 }
